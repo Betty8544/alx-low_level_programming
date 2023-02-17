b74c2a890bc76898d2e722fe69c9016d4b9079ad
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-02-17 03:35:41 -0800

    My commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base16
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		putchar(b);
+	}
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
