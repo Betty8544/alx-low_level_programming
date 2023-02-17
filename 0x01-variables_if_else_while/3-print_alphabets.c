fc62ef661de26df56ddc0dd54b8cc38ffa7bbd27
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-02-17 02:32:46 -0800

    My commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints  alphabets
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		putchar(d);
+	}
+
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
