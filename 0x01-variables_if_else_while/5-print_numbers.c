fbdb1277e771b3769ba51c7ed790a0dc4881a862
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-02-17 02:50:21 -0800

    My commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
