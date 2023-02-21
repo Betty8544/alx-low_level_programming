b31c47510be6294bf762eda0767e0520e7752285
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-02-21 06:07:10 -0800

    My commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *  print_to_98 - prints time table of dimention
+ *  @n: Dimention of times table
+ *
+ *  Return: void
+ */
+void print_to_98(int n)
+{
+	int a;
+	int b;
+	int c;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			if (b == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				c = a * b;
+				_putchar((c < 100) ? ' ' : ('0' + c / 100));
+				_putchar((c <  10) ? ' ' : ('0' + c / 10 % 10));
+				_putchar('0' + c % 10);
+			}
+
+			if (b != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
