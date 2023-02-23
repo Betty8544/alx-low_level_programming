782fa430ee37f56a9bf402dfd3fc306b919ae687
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-02-22 21:31:04 -0800

    My commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: The digit to be computed
+ *
+ * Return: 1 if c is a digit
+ */
+int _isdigit(int c)
+{
+	for (c = 0; c <= 9; c++)
+	{
+		return (1);
+	}
+
+	return (0);
+}
