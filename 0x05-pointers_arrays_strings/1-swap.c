263476897269350d4a19df214631770d0eeb4813
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-02-27 03:55:30 -0800

    My commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
