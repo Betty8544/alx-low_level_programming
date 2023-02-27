6d066924da7942fa6042cf4e5ee8ed57106a7fe4
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-02-27 06:01:17 -0800

    My commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: The pointer that points to an integer
+ * @n: The integer
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+	{
+		printf("%d", a[b]);
+
+		if (b != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
