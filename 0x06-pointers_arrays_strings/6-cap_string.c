12d668ab300c401a1b8c27e408707555f025f3cf
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-03-02 02:12:07 -0800

    My commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: The string
+ *
+ * Return: A ponter
+ */
+char *cap_string(char *str)
+{
+	int b = 0;
+
+	while (str[b])
+	{
+		while (!(str[b] >= 'a' && str[b] <= 'z'))
+			b++;
+
+		if (str[b - 1] == ' ' ||
+			str[b - 1] == '\t' ||
+			str[b - 1] == '\n' ||
+			str[b - 1] == ',' ||
+			str[b - 1] == ';' ||
+			str[b - 1] == '.' ||
+			str[b - 1] == '!' ||
+			str[b - 1] == '?' ||
+			str[b - 1] == '"' ||
+			str[b - 1] == '(' ||
+			str[b - 1] == ')' ||
+			str[b - 1] == '{' ||
+			str[b - 1] == '}' ||
+			b == 0)
+			str[b] -= 32;
+
+		b++;
+	}
+
+	return (str);
+}
