3874333c12f5082ca390d26231d7c0991c74f140
bethelhem mesfin haile <bethelhemhaile8544@gmail.com>
2023-03-16 09:57:47 -0700

    Update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,51 +2,51 @@
 #include <stdlib.h>
 
 /**
-  * string_nconcat - ...
-  * @s1: ...
-  * @s2: ...
-  * @n: ...
+  * string_nconcat - concatinates strings
+  * @s1: the first string
+  * @s2: the second string
+  * @n: an integer
   *
-  * Return: ...
+  * Return: void
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i = 0, j = 0, k = 0, l = 0;
-    char *str;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[i])
-        i++;
+	while (s1[i])
+		i++;
 
-    while (s2[k])
-        k++;
+	while (s2[k])
+		k++;
 
-    if (n >= k)
-        l = i + k;
-    else
-        l = i + n;
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
 
-    str = malloc(sizeof(char) * l + 1);
-    if (str == NULL)
-        return (NULL);
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
 
-    k = 0;
-    while (j < l)
+	k = 0;
+	while (j < l)
 	{
-        if (j <= i)
-            str[j] = s1[j];
+		if (j <= i)
+			str[j] = s1[j];
 
-        if (j >= i)
+		if (j >= i)
 		{
-            str[j] = s2[k];
-            k++;
+			str[j] = s2[k];
+			k++;
 		}
-        j++;
+		j++;
 	}
-    str[j] = '\0';
-    return (str);
+	str[j] = '\0';
+	return (str);
 }
